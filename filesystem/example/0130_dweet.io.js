26b308b297e3ebeb4a4a5cdb84663464dd9879c3
Nat <nat@cmmc.io>
2018-08-04 13:07:22 +0700

    update 0130_dweet.io - clear display - change matrix.print(integer) to matrix.print(string)

--- filesystem/example/0130_dweet.io.js
+++ filesystem/example/0130_dweet.io.js
@@ -2,10 +2,13 @@ console.log("Dweet");
 
 setInterval(function() {
     var light = kidbright.ldr.read();
-	console.log("Value: " + light);
-	kidbright.matrix.print( light );
+    console.log("Value: " + light);
+    kidbright.matrix.clear();
+    kidbright.matrix.setCursor(0,0);
+    kidbright.matrix.print( ""+light );
+    kidbright.matrix.writeDisplay();
     kidbright.dweet.send(ESP32.getState().cpuid,"light",light);
-}, 15000);
+}, 5000);
 
 //https://dweet.io/get/dweets/for/my-thing-name
 

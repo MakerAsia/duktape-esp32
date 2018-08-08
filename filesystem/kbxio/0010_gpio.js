ce224724e22f24b8accd16036573b7a18ebff53a
Nat <nat@cmmc.io>
2018-08-08 12:41:11 +0700

    - fix kbxio blink example.

--- filesystem/kbxio/0010_gpio.js
+++ filesystem/kbxio/0010_gpio.js
@@ -1,11 +1,13 @@
 var I2C = require("i2c");
-log("Constants:");
-log("I2C_NUM_0: " + I2C.I2C_NUM_0);
-log("I2C_NUM_1: " + I2C.I2C_NUM_1);
-log("I2C_MODE_MASTER: " + I2C.I2C_MODE_MASTER);
-log("I2C_MODE_SLAVE: " + I2C.I2C_MODE_SLAVE);
-log("I2C_MASTER_READ: " + I2C.I2C_MASTER_READ);
-log("I2C_MASTER_WRITE: " + I2C.I2C_MASTER_WRITE);
+const ADDRESS = 0x34; 
+
+console.log("Constants:");
+console.log("I2C_NUM_0: " + I2C.I2C_NUM_0);
+console.log("I2C_NUM_1: " + I2C.I2C_NUM_1);
+console.log("I2C_MODE_MASTER: " + I2C.I2C_MODE_MASTER);
+console.log("I2C_MODE_SLAVE: " + I2C.I2C_MODE_SLAVE);
+console.log("I2C_MASTER_READ: " + I2C.I2C_MASTER_READ);
+console.log("I2C_MASTER_WRITE: " + I2C.I2C_MASTER_WRITE);
 
 var i2cDevice = new I2C({
     port: I2C.I2C_NUM_0,
@@ -15,20 +17,25 @@ var i2cDevice = new I2C({
     master_clk_speed: 100000
 });
 
-const ADDRESS = 0x34; 
 i2cDevice.beginTransaction(ADDRESS); 
 var b = new Buffer(2)
 b[0] = 0x00;
-b[1] = 0x00
-i2cDevice._write(ADDRESS, 0x20, b)
+b[1] = 0x00;
+i2cDevice._write(ADDRESS, 0x20, b);
 
 b[0] = 0xff;
-b[1] = 0xff
-i2cDevice._write(ADDRESS, 0x21, b)
-i2cDevice._write(ADDRESS, 0x22, b)
+b[1] = 0xff;
+i2cDevice._write(ADDRESS, 0x21, b);
+i2cDevice._write(ADDRESS, 0x22, b);
 
-setTimeout(function() {
-    b[0] ^= b[0];
-    b[1] ^= b[1];
-    i2cDevice._write(ADDRESS, 0x22, b)
-}, 1000)
+setInterval(function() {
+    console.log("Free heap: " + ESP32.getState().heapSize);
+    b[0] = 0xff;
+    b[1] = 0xff;
+    i2cDevice._write(ADDRESS, 0x22, b);
+    setTimeout(function() {
+        b[0] ^= b[0];
+        b[1] ^= b[1];
+        i2cDevice._write(ADDRESS, 0x22, b);
+    }, 1000)
+}, 2000)

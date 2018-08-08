af689974d3a840f7b1fa94309daae736377529df
Nat <nat@cmmc.io>
2018-08-08 15:44:36 +0700

    - use Uint8Array

--- filesystem/kbxio/0010_gpio.js
+++ filesystem/kbxio/0010_gpio.js
@@ -19,27 +19,23 @@ const i2cDevice = new I2C({
 
 i2cDevice.beginTransaction(ADDRESS);
 i2cDevice.write(0x20);
-i2cDevice.write(0x00);
-i2cDevice.write(0x00);
+i2cDevice.write(new Uint8Array([0x00, 0x00]));
 i2cDevice.endTransaction();
 
 i2cDevice.beginTransaction(ADDRESS);
 i2cDevice.write(0x21);
-i2cDevice.write(0xff);
-i2cDevice.write(0xff);
+i2cDevice.write(new Uint8Array([0xff, 0xff]));
 i2cDevice.endTransaction();
 
 kidbright.loop(function () {
   i2cDevice.beginTransaction(ADDRESS);
   i2cDevice.write(0x22);
-  i2cDevice.write(0xff);
-  i2cDevice.write(0xff);
+  i2cDevice.write(new Uint8Array([0xff, 0xff]));
   i2cDevice.endTransaction();
   kidbright.delay(1000);
   i2cDevice.beginTransaction(ADDRESS);
   i2cDevice.write(0x22);
-  i2cDevice.write(0x00);
-  i2cDevice.write(0x00);
+  i2cDevice.write(new Uint8Array([0x00, 0x00]));
   i2cDevice.endTransaction();
   kidbright.delay(1000);
 });
